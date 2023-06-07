a0f153f4600493d1839a1732373376c3d20b0de8
Chappy0710 <111452188+Chappy0710@users.noreply.github.com>
2023-06-07 09:40:53 -0600

    Creacion del proyecto inicial

---
+++ src/main/java/com/TallerMemo/TallerMemoApplication.java
@@ -0,0 +1,13 @@
+package com.TallerMemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallerMemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallerMemoApplication.class, args);
+	}
+
+}
